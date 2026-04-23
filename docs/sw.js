0c592fd7aa7b95fa7a5a303bbf5e116427f9fdb3
Duver0 <duversiro@gmail.com>
2026-04-23 14:29:51 -0500

    feat: convertir en PWA con manifest y service worker

---
+++ docs/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'nomina-psp-v1';
+const urlsToCache = [
+  '/Auto-nomina/',
+  '/Auto-nomina/index.html',
+  '/Auto-nomina/downloads/nomina1-abril-2026.pdf',
+  '/Auto-nomina/downloads/nomina2-abril-2026.pdf'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
